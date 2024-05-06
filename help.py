85982f65de3325b8502758c3c950affe817cbe35
Oksana57 <114564080+Oksana57@users.noreply.github.com>
2024-05-06 11:34:59 +0300

    Update help.py

--- help.py
+++ help.py
@@ -1,47 +1,27 @@
-#import itertools
-from DZ6.chess.DZ6_3 import generate_queens
-from DZ6.chess.DZ6_2 import queens
-# f = [(0, 3), (1, 7), (2, 0), (3, 2), (4, 5), (5, 1), (6, 6), (7, 4)]
-# hit = False
+import os
+import json
+import csv
+import pickle
 
-# for i in range(8):
-#     x1 = f[i][0]
-#     y1 = f[i][1]
-#     for ii in range(i+1, 8):
-#         x2 = f[ii][0]
-#         y2 = f[ii][1]
-#         if x1 == x2 or y1 == y2 or abs(y2 - y1) == abs(x2 - x1):
-#             hit = True
-# if hit:
-#     print('YES')
-# else:
-#     print('NO')
 
-queens_list = []
-#list3 = []
-#while len(queens_list) <= 3:
-    
-f = generate_queens()
-#try :
-hit = False
+def write_json_csv_pickle(directory):
+    # write_JSON
+    result_dict_json = {}
+    for dir_path, dir_file, file_name in os.walk(directory):
+        result_dict_json[f'DIRECTORY - {dir_path}'] = [
+            f'FILE - {i} = {os.path.getsize(os.path.abspath(dir_path + "/" + i))} byte' for i in file_name]
+    with open('json_file.json', 'w', encoding='utf-8') as json_file:
+        json.dump(result_dict_json, json_file, indent=4, separators=(',', ':'))
+    # write_CSV
+    data = [["Dir", "Files"]]
+    for key, value in result_dict_json.items():
+        data.append([key, value])
+    with open('csv_file.csv', 'w', encoding='utf-8') as csv_f:
+        write_csv = csv.writer(csv_f, dialect='excel-tab', delimiter=',')
+        write_csv.writerows(data)
+    # write PICKLE
+    with open('pickle_file.bin', 'wb') as pickle_file:
+        pickle.dump(result_dict_json, pickle_file)
 
-for i in range(8):
-    x1 = f[i][0]
-    y1 = f[i][1]
-    for ii in range(i+1, 8):
-        x2 = f[ii][0]
-        y2 = f[ii][1]
-        if x1 == x2 or y1 == y2 or abs(y2 - y1) == abs(x2 - x1):
-            hit = True
-if hit:
-    print(f)
-    print('YES')
-else:
-    print(f)
-    print('NO')
-#     if queens(var1) is True:
-#         continue    
-#     else:
-#         queens_list.append(var1) 
 
-# print(queens_list)
+write_json_csv_pickle(directory='directory')
